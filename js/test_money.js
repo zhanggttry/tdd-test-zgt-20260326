3d3d1aba8b9f1e5009478d658254bddbd8e62816
zhanggttry <zhanggttry@163.com>
2026-03-23 23:35:19 +0800

    JS消除测试中的重复OK

--- js/test_money.js
+++ js/test_money.js
@@ -3,25 +3,21 @@ const assert = require('assert');
 const Money = require('./money');
 const Portfolio = require('./portfolio');
 
-// 测试代码1
-let fiveDollars = new Money(5, "USD");
-let tenDollars = new Money(10, "USD");
-assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
-
-// 测试代码2
+// 测试代码1，针对乘法
 let tenEuros = new Money(10, "EUR");
 let twentyEuros = new Money(20, "EUR");
-assert.strictEqual(twentyEuros.amount, 20);
-assert.strictEqual(twentyEuros.currency, "EUR");
+assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
 
-// 测试代码3，针对除法
+// 测试代码2，针对除法
 let originalMoney = new Money(4002, "KRW")
 let actualMoneyAfterDivision = originalMoney.divide(4)
 let expectedMoneyAfterDivision = new Money(1000.5, "KRW")
 assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision)
 
-// 测试代码3
-let fifteenDollars = new Money(15, "USD");
+// 测试代码3，针对汇率转换
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD")
+let fifteenDollars = new Money(15, "USD")
 let portfolio = new Portfolio();
 portfolio.add(fiveDollars, tenDollars);
 assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
