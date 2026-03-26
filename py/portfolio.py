f6ec8ab00d7d66c2ac00a948c7c267fa420b9b26
zhanggttry <zhanggttry@163.com>
2026-03-26 23:11:59 +0800

    Python测试OK

--- py/portfolio.py
+++ py/portfolio.py
@@ -11,10 +11,12 @@ class Portfolio:
         
     # 编写__convert方法
     def __convert(self,aMoney, aCurrency):
+        exchangeRates = {'EUR->USD': 1.2, 'USD->KRW': 1100} # 加一个字典
         if aMoney.currency == aCurrency:
             return aMoney.amount
         else:
-            return aMoney.amount * self._eur_to_usd
+            key = aMoney.currency + '->' + aCurrency
+            return aMoney.amount * exchangeRates[key]
         
     def add(self, *moneys):
         self.moneys.extend(moneys)
